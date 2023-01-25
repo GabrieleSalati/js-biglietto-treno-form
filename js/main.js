7dc5fc1171c16c9d8256d2b80a7496a917832f5e
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-25 19:29:02 +0100

    Graphic elements added

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@ const inputAge = document.getElementById("customerAge");
 
 console.log(customerAge);
 
+let randomCarriage = Math.floor(Math.random() * 15) + 1;
+
+let randomCode = Math.floor(Math.random() * 30000) + 1;
+
 // CALC FORMULA
 
 go.addEventListener(
@@ -45,8 +49,8 @@ go.addEventListener(
         }
 
         document.getElementById("userData").innerHTML = user;
-        document.getElementById("trip").innerHTML = length;
-        document.getElementById("userAge").innerHTML = age;
+        document.getElementById("carriage").innerHTML = randomCarriage;
+        document.getElementById("code").innerHTML = randomCode;
         document.getElementById("result").innerHTML = price;
 
         console.log(price);

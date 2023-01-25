7a18fc229ff649b1e8b7d531a735ead89dd53e07
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-25 18:44:06 +0100

    Data collection and calc formula implemented, display options added

--- js/main.js
+++ js/main.js
@@ -1,44 +1,57 @@
 // VARIABLES
 
-let user = document.getElementById("userName").value;
-
-document.getElementById("userData").innerHTML = user
+let inputName = document.getElementById("userName");
 
 console.log(userData);
 
-const length = document.getElementById("tripLength").value;
-
-document.getElementById("trip").innerHTML = length
+const inputLength = document.getElementById("tripLength");
 
 console.log(tripLength);
 
-const age = document.getElementById("customerAge").value;
-
-document.getElementById("userAge").innerHTML = age
+const inputAge = document.getElementById("customerAge");
 
 console.log(customerAge);
 
 // CALC FORMULA
 
-if (age < 18) {
-    price = length * 0.21 * (1 - 20 / 100);
-}
-
-else if (age >= 65) {
-    price = length * 0.21 * (1 - 40 / 100);
-}
-
-else {
-    price = length * 0.21;
-}
-
-console.log(price);
-
-// DATA PRINTING
-
 go.addEventListener(
     "click",
     function () {
+
+        // Richiesta nome
+
+        let user = inputName.value;
+
+        // Richiesta lunghezza viaggio
+
+        const length = inputLength.value;
+
+        // Richiesta eta' passeggero
+
+        const age = inputAge.value;
+
+        // CALC FORMULA
+
+        if (age < 18) {
+            price = length * 0.21 * (1 - 20 / 100);
+        }
+
+        else if (age >= 65) {
+            price = length * 0.21 * (1 - 40 / 100);
+        }
+
+        else {
+            price = length * 0.21;
+        }
+
+        document.getElementById("userData").innerHTML = user;
+        document.getElementById("trip").innerHTML = length;
+        document.getElementById("userAge").innerHTML = age;
         document.getElementById("result").innerHTML = price;
+
+        console.log(price);
     }
 );
+
+// DISPLAY VALUES
+
